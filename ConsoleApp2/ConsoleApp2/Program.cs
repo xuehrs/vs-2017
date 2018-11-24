8d438599cb4cfcc1b0ee73fec5e6cb8d2cad7a52
maylihaidong <maylihaidong@outlook.com>
2018-11-24 15:04:39 +0800

    解题

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -88,19 +88,45 @@ namespace ConsoleApp2
             //Console.WriteLine($"The range of the decimal type is {min} to {max}");
 
 
-            double a = 1.0;
-            double b = 3.0;
-            Console.WriteLine(a / b);
-            Console.WriteLine(-a / b);
+            //double a = 1.0;
+            //double b = 3.0;
+            //Console.WriteLine(a / b);
+            //Console.WriteLine(-a / b);
 
-            decimal c = 1.0M;
-            decimal d = 3.0M;
-            Console.WriteLine(c / d);
-            Console.WriteLine(-c / d);
+            //decimal c = 1.0M;
+            //decimal d = 3.0M;
+            //Console.WriteLine(c / d);
+            //Console.WriteLine(-c / d);
+
+
+            /*
+            请编写代码来计算圆面积（其中，半径为 2.50 厘米）。
+            请注意，圆面积是用半径的平方乘以 PI 进行计算。
+            小提示：.NET 包含 PI 常数 Math.PI，可用于相应的值计算。
+            */
+
+            
+            decimal r = 2.50M;
+            double R = 2.50;
+            double PI = Math.PI;
+            decimal pi = (decimal)PI;
+            Console.WriteLine(Math.PI);
+            decimal S = pi * r * r;
+            double SS = PI * R * R;
+            Console.WriteLine("高精度计算方式: "+S);
+            Console.WriteLine(SS);
 
 
 
-            Console.Write("Press any key to continue...");
+
+
+
+
+
+
+
+
+            Console.Write("press any key to continue...");
             Console.ReadKey(true);
         }
     }
