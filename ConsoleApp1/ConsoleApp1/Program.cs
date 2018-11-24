baa05dbab045ea6d6e3aa51cfd912462a90b8ef2
maylihaidong <maylihaidong@outlook.com>
2018-11-25 02:05:26 +0800

    添加笔记

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,6 +18,7 @@ namespace ConsoleApp1
             String aFriend1 = "Maira";
             Console.WriteLine(aFriend1);
             Console.WriteLine("Hello" + aFriend1);
+            //这个无效
             Console.WriteLine($"Hello aFriend");
             Console.WriteLine($"Hello {aFriend}");
 
@@ -26,12 +27,14 @@ namespace ConsoleApp1
             string firstFriend = "Maria";
             string secondFriend = "Sage";
             Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+            //length获得字符串的长度
             Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
             Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
             Console.WriteLine($"Hello123 {firstFriend}");
 
 
 
+            //删除字符串的空格相关
             string greeting = "      Hello World!       ";
             Console.WriteLine($"[{greeting}]");
 
@@ -46,16 +49,18 @@ namespace ConsoleApp1
 
 
 
+            //替换字符串
             string sayHello = "Hello World!";
             Console.WriteLine(sayHello);
             sayHello = sayHello.Replace("Hello", "Greetings");
             Console.WriteLine(sayHello);
-
+            //全部转大写或小写
             Console.WriteLine(sayHello.ToUpper());
             Console.WriteLine(sayHello.ToLower());
 
 
 
+            //判断字符窜位置是否符合预期
             string songLyrics = "You say goodbye, and I say hello";
             Console.WriteLine(songLyrics.Contains("goodbye"));
             Console.WriteLine(songLyrics.Contains("greetings"));
@@ -66,7 +71,7 @@ namespace ConsoleApp1
             Console.WriteLine(songLyrics.EndsWith("goodbye"));
 
 
-
+            //命令窗口不要太快消失
             Console.Write("Press any key to continue...");
             Console.ReadKey(true);
         }
