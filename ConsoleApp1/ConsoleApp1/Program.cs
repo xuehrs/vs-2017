04bca9537393331e4a486c40368cc8492403c67a
maylihaidong <maylihaidong@outlook.com>
2018-11-24 13:46:37 +0800

    测试+1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
         }
     }
 }
