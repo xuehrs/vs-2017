13954620b8e117d6c444b49bd37eab4bd9518480
maylihaidong <maylihaidong@outlook.com>
2018-11-25 05:20:43 +0800

    测试模板

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+            //在屏幕显示提示
+            Console.Write("按任意键自动消失bash窗口...");
+            //表示不管按什么键都会消失
+            Console.ReadKey(true);
+        }
+    }
+}
