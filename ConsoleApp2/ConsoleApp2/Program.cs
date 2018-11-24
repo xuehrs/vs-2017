4ea676a061eea3888c27c5cfe695354926888aab
maylihaidong <maylihaidong@outlook.com>
2018-11-24 14:29:49 +0800

    学习完毕

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -25,12 +25,78 @@ namespace ConsoleApp2
 
 
 
+            //int a = 5;
+            //int b = 4;
+            //int c = 2;
+            ////int d = (a + b) * c;
+            //int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            //Console.WriteLine(d);
 
 
 
+            //int a = 7;
+            //int b = 4;
+            //int c = 3;
+            //int d = (a + b) / c;
+            //Console.WriteLine(d);
 
 
 
+            //int a = 7;
+            //int b = 4;
+            //int c = 3;
+            //int d = (a + b) / c;
+            //int e = (a + b) % c;
+            //Console.WriteLine($"quotient: {d}");
+            //Console.WriteLine($"remainder: {e}");
+
+
+
+            //int max = int.MaxValue;
+            //int min = int.MinValue;
+            //Console.WriteLine($"The range of integers is {min} to {max}");
+
+            //int what = max + 3;
+            //Console.WriteLine($"An example of overflow: {what}");
+
+
+
+            //double a = 5;
+            //double b = 4;
+            //double c = 2;
+            //double d = (a + b) / c;
+            //Console.WriteLine(d);
+
+
+            //double a = 19;
+            //double b = 23;
+            //double c = 8;
+            //double d = (a + b) / c;
+            //Console.WriteLine(d);
+
+
+            //double max = double.MaxValue;
+            //double min = double.MinValue;
+            //Console.WriteLine($"The range of double is {min} to {max}");
+
+            //double third = 1.0 / 3.0;
+            //Console.WriteLine(third);
+
+
+            //decimal min = decimal.MinValue;
+            //decimal max = decimal.MaxValue;
+            //Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+            Console.WriteLine(-a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+            Console.WriteLine(-c / d);
 
 
 
