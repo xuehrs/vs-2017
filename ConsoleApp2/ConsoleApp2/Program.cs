ac9039b74cccd0c7dcd458173c44fc33b5b24c99
maylihaidong <maylihaidong@outlook.com>
2018-11-24 13:51:32 +0800

    处理 C# 中的整数和浮点数

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
